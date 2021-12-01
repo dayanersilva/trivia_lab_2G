804e3f306ffc65ce58c4d5e605e51bd643baaaca
dayanersilva <dayane.rodrigues_silva@hotmail.com>
2021-12-01 15:20:11 -0300

    CSS finalizado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function enviar(){
     //alert ('Resultado: Questao 1 - letra A. Questão 2 - letra C. Questão 3 - Letra C .!')
    //document.body.innerHTML = ""
-   document.getElementById('res').innerHTML = "<b>Resultado: Questao 1 - letra A. Questão 2 - letra C. Questão 3 - Letra C .!</b>"
+   document.getElementById('res').innerHTML = "<b>Gabarito: Questao 1 - letra A. Questão 2 - letra C. Questão 3 - Letra C .</b>"
 }
